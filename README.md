a30368b9e55e165ab15a5871b40deb7981fa4b18
Kavya Varma <kavyabu@gmail.com>
2020-09-22 18:37:28 +1000

    updated readme

--- README.md
+++ README.md
@@ -2,12 +2,39 @@
 Transform Fixed Width Text to Delimited
 
 ###Installation
+Wheel
 ```
+    pip install  fwt2delimited-1.0.0-py3-none-any.whl
 ```
 
-###Usage
+PyPi
+```
+
+```
+
+###Usage samples
 ```
 from fwt2delimited.fwt import Fwt
 ...
-fwt_obj = Fwt(spec)
+# construct Fwt with the spec mentioned
+fwt = Fwt(spec) 
+
+# construct Fwt with the default spec
+fwt = Fwt()
+ 
+# according to the spec mentioned, 
+# generate FWT - file name "fwt_file.txt" in current dir, using random dataset 
+fwt_fname = fwt.generate_fwt_file() 
+
+# generate FWT - file name in param "file_name" in current dir, default sample dataset  
+fwt_fname = fwt.generate_fwt_file(random_data=False, file_name=<filename>) 
+
+# generate FWT - file name "fwt_file.txt" in current dir, sample dataset in the param "sample_values_json_str" 
+fwt_fname = fwt.generate_fwt_file(random_data=False, sample_values_json_str=<jsonstr>)
+
+# convert FWT to delimited - default delimiter ',' , output file name "delimited_file.csv"
+delim_fname = fwt.fwt_to_delimited(fwt_fname)
+
+# convert FWT to delimited - delimiter and output file name specified in the param
+delim_fname = fwt.fwt_to_delimited(fwt_fname, delimited_file=<>)
 ```
--- fwt2delimited/fwt_utils.py
+++ fwt2delimited/fwt_utils.py
@@ -116,7 +116,7 @@ class Fwt:
         else:
             return lines
 
-    def generate_fwt_file(self, random_data=True, num_of_records: int = 20, file_path: str = "fwt_output.txt",
+    def generate_fwt_file(self, random_data=True, num_of_records: int = 20, file_name: str = "fwt_file.txt",
                           sample_values_json_str: str = get_sample_values("")) -> str:
         try:
             if random_data is True:
@@ -124,7 +124,7 @@ class Fwt:
             else:
                 lines = self.generate_dataset_from_sample(num_of_records, sample_values_json_str)
 
-            fwt_file = open(file_path, "w", encoding=self.spec.encoding_format_fwt)
+            fwt_file = open(file_name, "w", encoding=self.spec.encoding_format_fwt)
             fwt_file.write('\n'.join(lines))
         except (IOError, ValueError, KeyError, LookupError, FileNotFoundError):
             logging.error("Error while generating FWT file.")
@@ -132,7 +132,7 @@ class Fwt:
         else:
             return fwt_file.name
 
-    def fwt_to_delimited(self, fwt_file: str, delimiter: str = ',', delimited_file: str = "data/delimited.csv") -> str:
+    def fwt_to_delimited(self, fwt_file: str, delimiter: str = ',', delimited_file: str = "delimited_file.csv") -> str:
         try:
             records_fwt = self.get_records_fwt_file(fwt_file)
             records_delimited = self.convert_records(records_fwt, delimiter)
